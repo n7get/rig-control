ebc76ec532799ffbd53118aa9ab34b1af9eaed85
Robert Ambrose <rna@muttsoft.com>
2025-05-02 06:25:45 -0700

    Mostly renamed network_ to tcp_ in network.c

--- main/network.c
+++ main/network.c
@@ -20,6 +20,7 @@
 #define KEEPALIVE_INTERVAL          5
 #define KEEPALIVE_COUNT             3
 #define NETWORK_RECV_QUEUE_SIZE     10
+#define TCP_PORT 9911
 
 typedef struct {
     int channel;
@@ -28,19 +29,19 @@ typedef struct {
 } send_queue_buf_t;
 
 static const char *TAG = "NETWORK";
-static QueueHandle_t send_queue = NULL;
+static QueueHandle_t tcp_send_queue = NULL;
 
 // Callback function to put received rig data onto the network send queue
-static void network_send_callback(void *context, void *data) {
+static void tcp_send_callback(void *context, void *data) {
     result_buf_t *result = (result_buf_t *)data;
 
     send_queue_buf_t send_buf;
     send_buf.channel = *(int *)context;
     strncpy(send_buf.data, result->data, RECV_BUFFER_SIZE);
     send_buf.len = result->len;
-    // ESP_LOGI(TAG, "Queuing data, channel: %d, data: %s, len: %d", send_buf.channel, send_buf.data, send_buf.len);
+    // ESP_LOGI(TAG, "Queuing TCP data, channel: %d, data: %s, len: %d", send_buf.channel, send_buf.data, send_buf.len);
 
-    if (xQueueSend(send_queue, &send_buf, pdMS_TO_TICKS(100)) != pdPASS) {
+    if (xQueueSend(tcp_send_queue, &send_buf, pdMS_TO_TICKS(100)) != pdPASS) {
         ESP_LOGE(TAG, "Failed to queue network send data");
     }
 }
@@ -97,7 +98,7 @@ static void connection_established(int sock, buffer_t *buffer) {
                 rx_buffer[i + 1] = '\0';
 
                 if (strcmp(rx_buffer, ENHANCED_RIG_COMMAND_ENHANCED_MODE) == 0) {
-                    rig_monitor_add_observers(OBSERVE_UPDATES|OBSERVE_STATUS, network_send_callback, (void *)&sock);
+                    rig_monitor_add_observers(OBSERVE_UPDATES|OBSERVE_STATUS, tcp_send_callback, (void *)&sock);
                 } else {
                     rig_monitor_send(rx_buffer, SEND_TYPE_COMMAND);
                 }
@@ -141,13 +142,13 @@ static void wait_for_accept(int listen_sock) {
         // Set the current active socket for the send task
         ESP_LOGI(TAG, "Socket accepted");
 
-        rig_monitor_add_observers(OBSERVE_COMMANDS, network_send_callback, (void *)&sock);
+        rig_monitor_add_observers(OBSERVE_COMMANDS, tcp_send_callback, (void *)&sock);
 
         buffer_t *buffer = buffer_create(RECV_BUFFER_SIZE, buffer_fill_callback, (void *)&sock);
         if (buffer == NULL) {
             ESP_LOGE(TAG, "Failed to create buffer");
             close(sock);
-            rig_monitor_remove_observers(network_send_callback);
+            rig_monitor_remove_observers(tcp_send_callback);
             return;
         }
 
@@ -157,13 +158,13 @@ static void wait_for_accept(int listen_sock) {
         shutdown(sock, 0);
         close(sock);
         buffer_free(buffer);
-        rig_monitor_remove_observers(network_send_callback);
+        rig_monitor_remove_observers(tcp_send_callback);
         ESP_LOGI(TAG, "Socket closed");
     }
 }
 
 // Task to handle incoming TCP connections and receive data
-static void recv_task(void *pvParameters) {
+static void tcp_recv_task(void *pvParameters) {
     int addr_family = AF_INET;
     int ip_protocol = IPPROTO_IP;
     struct sockaddr_storage dest_addr;
@@ -171,7 +172,7 @@ static void recv_task(void *pvParameters) {
     struct sockaddr_in *dest_addr_ip4 = (struct sockaddr_in *)&dest_addr;
     dest_addr_ip4->sin_addr.s_addr = htonl(INADDR_ANY);
     dest_addr_ip4->sin_family = AF_INET;
-    dest_addr_ip4->sin_port = htons(NETWORK_PORT);
+    dest_addr_ip4->sin_port = htons(TCP_PORT);
 
     int listen_sock = socket(addr_family, SOCK_STREAM, ip_protocol);
     if (listen_sock < 0) {
@@ -188,7 +189,7 @@ static void recv_task(void *pvParameters) {
         ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
         return;
     }
-    ESP_LOGI(TAG, "Socket bound, port %d", NETWORK_PORT);
+    ESP_LOGI(TAG, "Socket bound, port %d", TCP_PORT);
 
     wait_for_accept(listen_sock);
 
@@ -197,12 +198,13 @@ static void recv_task(void *pvParameters) {
 }
 
 // Task to send data from the queue to the connected TCP client
-static void send_task(void *pvParameters) {
+static void tcp_send_task(void *pvParameters) {
     while (1) {
         send_queue_buf_t send_buf;
 
         // Wait for data to appear in the queue
-        if (xQueueReceive(send_queue, &send_buf, portMAX_DELAY) == pdPASS) {
+        if (xQueueReceive(tcp_send_queue, &send_buf, portMAX_DELAY) == pdPASS) {
+            // ESP_LOGI(TAG, "Sending data, channel: %d, data: %s, len: %d", send_buf.channel, send_buf.data, send_buf.len);
             int written = send(send_buf.channel, send_buf.data, send_buf.len, 0);
             if (written < 0) {
                 ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
@@ -214,14 +216,14 @@ static void send_task(void *pvParameters) {
 
 // Initialize network tasks and queue
 esp_err_t network_init(void) {
-    send_queue = xQueueCreate(300, sizeof(send_queue_buf_t));
-    if (send_queue == NULL) {
+    tcp_send_queue = xQueueCreate(300, sizeof(send_queue_buf_t));
+    if (tcp_send_queue == NULL) {
         ESP_LOGE(TAG, "Failed to create network receive queue");
         return ESP_FAIL;
     }
 
-    xTaskCreate(recv_task, "network_recv_task", 4096, NULL, 5, NULL);
-    xTaskCreate(send_task, "network_send_task", 4096, NULL, 5, NULL);
+    xTaskCreate(tcp_recv_task, "tcp_recv_task", 4096, NULL, 5, NULL);
+    xTaskCreate(tcp_send_task, "tcp_send_task", 4096, NULL, 5, NULL);
 
     ESP_LOGI(TAG, "Network tasks initialized");
     return ESP_OK;
--- main/network.h
+++ main/network.h
@@ -4,8 +4,6 @@
 #include "esp_err.h"
 #include "observer.h" // For result_buf_t
 
-#define NETWORK_PORT 9911
-
 // Initialize network tasks and queues
 esp_err_t network_init(void);
 
