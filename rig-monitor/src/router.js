466e99886fa6ca8ad3ac4828883d1220f51621f7
Robert Ambrose <rna@muttsoft.com>
2025-06-13 05:37:17 -0700

    A trivial change to rename view -> views and optimize router.js a bit.

--- rig-monitor/src/router.js
+++ rig-monitor/src/router.js
@@ -1,34 +1,30 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
 
-import ui from '@/view/ui-view.vue'
-import opModes from '@/view/op-modes-view.vue'
-import opModesEdit from '@/view/op-mode-edit-view.vue'
-
 const routes = [
   {
       path: '/',
-      component: ui,
+      component: () => import('@/views/ui-view.vue'),
   },
   {
       path: '/op_modes',
       name: 'op_modes',
-      component: opModes,
+      component: () => import('@/views/op-modes-view.vue'),
   },
   {
       path: '/op_mode_edit/:edit/:id?',
       name: 'op_mode_edit',
-      component: opModesEdit,
+      component: () => import('@/views/op-mode-edit-view.vue'),
       props: true,
   },
   {
     path: '/mem_chans',
     name: 'mem_chans',
-    component: () => import('@/view/mem-chans-view.vue'),
+    component: () => import('@/views/mem-chans-view.vue'),
   },
   {
     path: '/mem_chan_edit/:edit/:id?',
     name: 'mem_chan_edit',
-    component: () => import('@/view/mem-chan-edit-view.vue'),
+    component: () => import('@/views/mem-chan-edit-view.vue'),
     props: true,
   },
 ]
